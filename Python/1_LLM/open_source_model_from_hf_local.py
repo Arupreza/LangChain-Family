e8f8b8068b7b3c08d9ef02b33959b9778aff4bdc
Arupreza <arupreza@sch.ac.kr>
2025-09-04 15:10:02 +0900

    push 4

--- Python/1_LLM/open_source_model_from_hf_local.py
+++ Python/1_LLM/open_source_model_from_hf_local.py
@@ -1,21 +1,24 @@
 # . check the mode available in the local machine 
 # ls ~/.cache/huggingface/hub
 
+gemma_model = "google/gemma-2-2b-it"
+llama_model = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+
 from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
 
-# Build the Hugging Face pipeline wrapper
 llm = HuggingFacePipeline.from_model_id(
-    model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    model_id=gemma_model,
     task="text-generation",
     pipeline_kwargs=dict(
         temperature=0.5,
-        max_new_tokens=100,   # <-- correct key
+        max_new_tokens=100,
+        return_full_text=False,
     )
 )
 
-# Wrap in a chat interface
-model = ChatHuggingFace(llm=llm)
+model = ChatHuggingFace(llm=llm, model_id=gemma_model)
 
-# Run inference
 result = model.invoke("What is the capital of Bangladesh?")
-print(result.content)   # .content gives just the text output
+print(result.content)
+
+
